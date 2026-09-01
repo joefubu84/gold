6001e8ed9a31b6952e26db137b00d46897fc4a82
joefubu84 <joefubu84@users.noreply.github.com>
2026-09-01 13:03:07 +0800

    feat: install Monetag Multitag script and update service worker zone 11699088

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ﻿self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11699059
+    "zoneId": 11699088
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
